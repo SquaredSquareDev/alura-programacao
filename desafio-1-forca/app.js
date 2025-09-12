5de0b64679807a734ea9ed17599a19c2a91629f4
SquaredSquareDev <dmssantos09@gmail.com>
2025-09-12 18:55:59 -0300

    imagens funcionando, arrumar botão de reniniciar depois

--- desafio-1-forca/app.js
+++ desafio-1-forca/app.js
@@ -1,8 +1,9 @@
 // Declarar variaveis
 let palavraSecreta;
 let letrasChutadas = [];
-let tentativasRestantes = 6;
 let mostrarChutes = document.getElementById('letrasChutadas');
+let imagemErros = 0;
+let imagem = `imagens/${imagemErros} erros.jpg`;
 
 // Verifica se o array tem valores duplicados
 function temDuplicado(arr) {
@@ -22,6 +23,7 @@ function escolherPalavraSecreta() {
         document.querySelector('.entrada-palavra-secreta').style.display = 'none';
         document.querySelector('.box-letras-digitadas').style.display = 'block';
         document.querySelector('.descoberta-palavra').style.display = 'block';
+        document.querySelector('img').setAttribute('src', imagem);
 
         // Mostra quantas letras tem
         for(let i = 0; i < palavraSecreta.length; i++){
@@ -33,6 +35,13 @@ function escolherPalavraSecreta() {
 
 }
 
+// Função para trocar de imagem
+function proximaImagem() {
+    imagemErros++;
+    imagem = `imagens/${imagemErros} erros.jpg`;
+    document.querySelector('img').setAttribute('src', imagem);
+}
+
 // O jogo em si (jogador 2 descobre a palavra)
 function chutarLetra() {
     let chute = document.querySelector('#letras').value;
@@ -42,7 +51,14 @@ function chutarLetra() {
          if (palavraSecreta.includes(chute)) {
             console.log('yay');
         } else {
-            console.log(':(');
+            if (imagemErros < 5) {    
+                proximaImagem();
+                console.log(':(');
+            } else {
+                proximaImagem();
+                document.querySelector('h1').innerHTML = "Você perdeu!";
+                document.getElementById('reiniciar').removeAttribute('disabled');
+            }
         }
     } else {
         alert('Escolha outra letra');
@@ -50,4 +66,15 @@ function chutarLetra() {
     }
 
     mostrarChutes.innerText = letrasChutadas.join(", ");
+}
+
+// Reinicia o jogo
+function reiniciar() {
+    imagemErros = 0;
+    letrasChutadas = [];
+    imagem = `imagens/${imagemErros} erros.jpg`;
+    palavraSecreta = null;
+    document.querySelector('.entrada-palavra-secreta').style.display = 'block';
+    document.querySelector('.box-letras-digitadas').style.display = 'none';
+    document.querySelector('.descoberta-palavra').style.display = 'none';
 }
