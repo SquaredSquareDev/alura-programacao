1de27325668dfbf0de27a201d5a0a464e4aca755
SquaredSquareDev <dmssantos09@gmail.com>
2025-09-15 23:49:53 -0300

    parte do botão reset feita, pequenas mudanças no codigo

--- desafio-1-forca/app.js
+++ desafio-1-forca/app.js
@@ -8,16 +8,33 @@ let imagem = `imagens/${imagemErros} erros.jpg`;
 
 // Verifica se o array tem valores duplicados
 function temDuplicado(arr) {
+
   return new Set(arr).size !== arr.length;
+
 }
+
+let myInput = document.querySelector('#palavra-secreta');
+let myButton = document.querySelector('#botao-secreto');
+
+myInput.addEventListener('keydown', function(event) {
+
+    if (event.key === "enter") {
+
+        myButton.click();
+
+    }
+
+});
 
 // Inicio do jogo (jogador 1 escolhe a palavra)
 function escolherPalavraSecreta() {
+
     // Pega a palavra escolhida
     let palavra = document.querySelector('#palavra-secreta').value;
     console.log(palavra);
 
     if (palavra != '') {
+
         palavraSecreta = palavra.split('');
         letrasDescobertas = palavraSecreta.map(function(letra) {
             return '_';
@@ -34,76 +51,141 @@ function escolherPalavraSecreta() {
 
         // Mostra quantas letras tem
         for(let i = 0; i < palavraSecreta.length; i++){
+
             document.querySelector('.letras-descobertas').insertAdjacentHTML("beforeend", '<span class="letra">_</span>');
+
         }
+
     } else {
+
         alert('Digite uma palavra');
+
     }
+
+    inputEnter('#palavra-secreta', '#botao-secreto');
 
 }
 
 // Função para trocar de imagem
 function proximaImagem() {
+
     imagemErros++;
     imagem = `imagens/${imagemErros} erros.jpg`;
     document.querySelector('img').setAttribute('src', imagem);
+
 }
 
 // O jogo em si (jogador 2 descobre a palavra)
 function chutarLetra() {
+
     let chute = document.querySelector('#letras').value;
     letrasChutadas.push(chute);
 
     if (temDuplicado(letrasChutadas) == false) {
+
          if (palavraSecreta.includes(chute)) {
+
             console.log('yay');
             atualizarLetrasDescobertas()
             verificaSeGanhou()
+
         } else {
+
             if (imagemErros < 5) {    
+
                 proximaImagem();
                 console.log(':(');
+
             } else {
+
                 proximaImagem();
                 document.querySelector('h1').innerHTML = "Você perdeu!";
                 document.getElementById('reiniciar').removeAttribute('disabled');
+
             }
+
         }
+        
     } else {
+
         alert('Escolha outra letra');
         letrasChutadas.pop();
+
     }
 
+    inputEnter('#letras', '#botao-letras');
+    limparCampo('#letras');
     mostrarChutes.innerText = letrasChutadas.join(", ");
+
 }
 
+// Atualiza as letras descobertas
 function atualizarLetrasDescobertas() {
 
     for (let i = 0; i < palavraSecreta.length; i++) {
+
         if (palavraSecreta[i] == letrasChutadas[letrasChutadas.length - 1]) {
             letrasDescobertas[i] = letrasChutadas[letrasChutadas.length - 1];
             document.querySelectorAll('.letra')[i].innerHTML = letrasDescobertas[i];
+
         }
+
     }
 
 }
 
+// Verifica se o jogador descobriu todas as letras
 function verificaSeGanhou() {
+
     if (letrasDescobertas.indexOf('_') == -1) {
+
         document.querySelector('h1').innerHTML = 'Parabéns, vocé ganhou!';
         document.getElementById('reiniciar').removeAttribute('disabled');
+
     }
+
+}
+
+//Limpa o campo
+function limparCampo(input) {
+
+    chute = document.querySelector(input);
+    chute.value = '';
+
 }
 
 // Reinicia o jogo
 function reiniciar() {
+
+    limparCampo('#palavra-secreta');
     imagemErros = 0;
     letrasChutadas = [];
+    palavraSecreta = [];
+    letrasDescobertas = [];
     imagem = `imagens/${imagemErros} erros.jpg`;
     palavraSecreta = null;
     document.querySelector('.entrada-palavra-secreta').style.display = 'block';
     document.querySelector('.box-letras-digitadas').style.display = 'none';
     document.querySelector('.descoberta-palavra').style.display = 'none';
+    document.querySelector('h1').innerHTML = 'Jogo da Forca';
+
+}
+
+function inputEnter(input, button) {
+
+    let myInput = document.querySelector(input);
+    let myButton = document.querySelector(button);
+
+    myInput.addEventListener('keydown', function(event) {
+
+        if (event.key === "enter") {
+
+            myButton.click();
+
+        }
+
+    });
+
 }
 
 // TODO: Fazer o reiniciar jogo funcionar
